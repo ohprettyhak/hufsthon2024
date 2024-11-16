a81b826317148a237f3921a0bb09897b3c61d397
ohprettyhak <haklee.googl@gmail.com>
2024-11-16 17:51:11 +0900

    feat: Add QR padding

--- src/pages/qrcode/index.tsx
+++ src/pages/qrcode/index.tsx
@@ -1,12 +1,18 @@
 import Layout from '@/components/Layout';
 
 import * as styles from './styles.css';
+import { rem } from '@/utils/pxto.ts';
 
 const QRCodePage = () => {
   return (
     <Layout bg="white">
       <div className={styles.root}>
-        <img className={styles.image} src="/static/image_route.png" alt="Route" />
+        <img
+          className={styles.image}
+          style={{ marginTop: rem(48) }}
+          src="/static/image_route.png"
+          alt="Route"
+        />
         <img className={styles.image} src="/static/image_qr.png" alt="QRCode" />
       </div>
     </Layout>
